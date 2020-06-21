a849977112a0a6458a6dc1a11258d29621daf9f4
Arthur Lemaire <arthur.n.lemaire@gmail.com>
2020-06-21 16:43:51 +0200

    Ajoutez des fichiers projet.

---
+++ Model/Vector.cs
@@ -0,0 +1,55 @@
+﻿using Anki.Vector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorControllerShell.Model
+{
+    public class Vector
+    {
+        public string Name { get; set; }
+        public bool Sleep { get; set; }
+        public bool isDocked { get; set; }
+        public bool isConnected { get; set; }
+
+        public List<Vector> CreateAllVector()
+        {
+            List<Vector> listeVector = new List<Vector>();
+
+            var listeStringVector = ConfigurationManager.AppSettings["ListeVector"].ToString().Split(';').ToList();
+            if (listeStringVector != null)
+            {
+                foreach (var vectorName in listeStringVector)
+                {
+                    listeVector.Add(new Vector() { Name = "Vector-" + vectorName, Sleep = false, isDocked = false, isConnected = false });
+                }
+            }
+            return listeVector;
+        }
+        public async Task UpdateVectorStatusAsync()
+        {
+            using (var robot = await Robot.NewConnection(this.Name))
+            {
+                this.isDocked = robot.Status.IsOnCharger;
+                this.Sleep = robot.Status.IsCharging;
+            }
+        }
+
+        public async Task GoToSleepAsync()
+        {
+            using (var robot = await Robot.NewConnection(this.Name))
+            {
+                if (await robot.Control.RequestControl())
+                {
+                    await robot.Behavior.DriveOnCharger();
+                    await robot.Behavior.Sleep();
+                    this.isDocked = true;
+                    this.Sleep = true;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorControllerShell.Model;
+
+namespace VectorControllerShell
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                var listeVector = new Vector().CreateAllVector();
+                Console.WriteLine("Begin Vector connection ...");
+                if (listeVector != null)
+                {
+                    Console.WriteLine("Vector fetching ...");
+                    foreach (var vector in listeVector)
+                    {
+                        Console.WriteLine($"Begin vector {vector.Name} ");
+                        await vector.UpdateVectorStatusAsync();
+                        Console.WriteLine($"Vector {vector.Name} is updated");
+                        await vector.GoToSleepAsync();
+                        if (vector.Sleep)
+                        {
+                            Console.WriteLine("Vector is sleeping");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
